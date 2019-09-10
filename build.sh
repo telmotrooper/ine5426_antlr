149e03527d8324834ffb011fdecacc85f516ca12
Telmo "Trooper <telmo.trooper@gmail.com>
2019-09-10 15:27:29 -0300

    Working on 'build.sh'

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+ANTLR=./antlr-4.7.2-complete.jar
+
 echo "Removing old files (if they exist)..."
 
 rm -rf .antlr *.java *.interp *.tokens *.class 2> /dev/null
@@ -10,4 +12,10 @@ antlr4 Expr.g4
 
 echo "Generating .class files..."
 
-javac -cp antlr-4.7.2-complete.jar Expr*.java
+javac Expr*.java
+# javac -cp $ANTLR Expr*.java
+
+echo "Write your expression and press Ctrl+D to see the results."
+
+grun Expr prog -gui
+# java -cp $ANTLR org.antlr.v4.gui.TestRig Expr prog -gui
