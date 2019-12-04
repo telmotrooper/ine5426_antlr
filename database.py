9c35077b967b5ce8400c5507c541d18706fbb037
Telmo "Trooper <telmo.trooper@gmail.com>
2019-12-03 23:15:30 -0300

    Added setScope

--- database.py
+++ database.py
@@ -1,10 +1,16 @@
 import sqlite3
 
-conn = sqlite3.connect(':memory:')
-# conn = sqlite3.connect('test.db')
+# conn = sqlite3.connect(':memory:')
+conn = sqlite3.connect('test.db')
 
 # Create table
 conn.execute('''
 CREATE TABLE IF NOT EXISTS symbols
 (id INTEGER, line INTEGER, column INTEGER, token TEXT, lexeme TEXT, scope TEXT)
 ''')
+
+def setScope(lexeme, scope):
+  conn.execute(f'''
+  UPDATE symbols SET scope = {scope} WHERE id = (SELECT id FROM symbols WHERE lexeme = '{lexeme}' AND scope IS NULL LIMIT 1)
+  ''')
+  conn.commit()
