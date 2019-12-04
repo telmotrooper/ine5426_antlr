f95a0501cce5d905b1fe439cee95fe90cbd33bbc
Telmo "Trooper <telmo.trooper@gmail.com>
2019-12-03 23:28:52 -0300

    Added checkForScopeError

--- database.py
+++ database.py
@@ -4,13 +4,20 @@ import sqlite3
 conn = sqlite3.connect('test.db')
 
 # Create table
-conn.execute('''
-CREATE TABLE IF NOT EXISTS symbols
-(id INTEGER, line INTEGER, column INTEGER, token TEXT, lexeme TEXT, scope TEXT)
-''')
+# conn.execute('''
+# CREATE TABLE IF NOT EXISTS symbols
+# (id INTEGER, line INTEGER, column INTEGER, token TEXT, lexeme TEXT, scope TEXT)
+# ''')
 
 def setScope(lexeme, scope):
   conn.execute(f'''
   UPDATE symbols SET scope = {scope} WHERE id = (SELECT id FROM symbols WHERE lexeme = '{lexeme}' AND scope IS NULL LIMIT 1)
   ''')
   conn.commit()
+
+def checkForScopeError(lexeme, scope):
+  c = conn.cursor()
+  c.execute(f'''SELECT COUNT(id) FROM symbols WHERE lexeme = '{lexeme}' AND scope = {scope}''')
+  if c.fetchone()[0] > 1:
+    return True
+  return False
